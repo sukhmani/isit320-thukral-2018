5420c0ee76e8c2e233c796628f3e6d4daf640d5b
sukhmanit <sukhmani@bellevuecollege.edu>
2018-10-16 00:12:00 +0000

    servet-gitignore

--- aws-provision/server/routes/index.js
+++ aws-provision/server/routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
+const getAwsInstanceParams = require('./aws/getAwsInstanceParams')
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) { 'use strict';
   res.render('index', { title: 'server' });
+});
+
+/* Set up a route called foo. */
+router.get('/create-educate', function(request, response) {
+  const awsInstanceParams = getAwsInstanceParams.awsEducate();
+  createInstance(awsInstanceParams);
+    //response.send(message);
 });
 
 module.exports = router;
