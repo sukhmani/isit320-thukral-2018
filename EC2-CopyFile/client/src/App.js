11647df77e0693a701bf3fe1904260c04e376210
charlie at school <sukhmani@bellevuecollege.edu>
2018-10-20 17:04:21 -0700

    button on App.js

--- EC2-CopyFile/client/src/App.js
+++ EC2-CopyFile/client/src/App.js
@@ -6,20 +6,15 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header>
+          <h1> Copy File </h1> 
         </header>
+        <main>
+          <button onClick={this.CopyFile}> Copy File </button>
+        </main>
+        <footer>
+          <p>&Copy; by Sukhmani</p>
+        </footer>
       </div>
     );
   }
