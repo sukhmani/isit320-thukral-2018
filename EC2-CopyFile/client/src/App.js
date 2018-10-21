1e08c8a4cae7ddc5ef966b884e97e6334a4008ff
charlie at school <sukhmani@bellevuecollege.edu>
2018-10-20 22:27:41 -0700

    testing copy-file

--- EC2-CopyFile/client/src/App.js
+++ EC2-CopyFile/client/src/App.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
 
-  copyFile = () =>{
+  copyFile = () => {
 
-    fetch('/foo')
-    .then(function(response)
-    {return response.json();
+    fetch('/script-pusher/copy-file')
+    .then(function(response){
+      return response.json();
     })
-    .then(function(json){
+    .then(function (json){
       console.log('parsed json', json);
     })
     .catch(function(ex){
