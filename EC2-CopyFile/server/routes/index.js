d4ca2116111c1f1f9d314934e16e89f9a6041ad3
charlie at school <sukhmani@bellevuecollege.edu>
2018-10-20 21:31:11 -0700

    test server method

--- EC2-CopyFile/server/routes/index.js
+++ EC2-CopyFile/server/routes/index.js
@@ -5,9 +5,9 @@ var router = express.Router();
 router.get('/', function(req, res, next) { 'use strict';
   res.render('index', { title: 'server' });
 });
-router.get('/foo', (request, response) =>{
-  const result = {result:'success'};
-  response.send(result);
-});
+// router.get('/foo', (request, response) =>{
+//   const result = {result:'success'};
+//   response.send(result);
+// });
 module.exports = router;
 
