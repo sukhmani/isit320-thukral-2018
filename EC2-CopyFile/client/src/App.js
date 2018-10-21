679ba05b916c115a39f686e72dc513f853e8ba9a
charlie at school <sukhmani@bellevuecollege.edu>
2018-10-20 19:49:30 -0700

    updated fetch in accordance to index

--- EC2-CopyFile/client/src/App.js
+++ EC2-CopyFile/client/src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
 
   copyFile = () =>{
 
-    fetch('/script-pusher/remove-known-host')
+    fetch('/foo')
     .then(function(response)
     {return response.json();
     })
