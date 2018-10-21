2db40e01928aa452c7edd43d397b5b3e3e07e983
charlie at school <sukhmani@bellevuecollege.edu>
2018-10-21 00:38:59 -0700

    Application Middleware - added app.use & require

--- week03-rest-basics/server/app.js
+++ week03-rest-basics/server/app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var api = require('./routes/api');  
 var app = express();
 
 // view engine setup
@@ -26,7 +26,7 @@ app.use('/users', usersRouter);
 app.use(function(req, res, next) { 'use strict';
   next(createError(404));
 });
-
+app.use('/api', api); 
 // error handler
 app.use(function(err, req, res, next) { 'use strict';
   // set locals, only providing error in development
