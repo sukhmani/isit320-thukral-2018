d5dc230003732405f874014415b1cbc513ad81e0
sukhmani <sukhmani@bellevuecollege.edu>
2018-10-22 15:17:22 -0700

    awsssh

---
+++ week05-ec2-copy-file/client/src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+//import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+    copyFile = () => {
+        //const that = this;
+        fetch('/script-pusher/foo')
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(json) {
+                console.log('parsed json', json);
+                //that.setState(foo => (json));
+            })
+            .catch(function(ex) {
+                console.log('parsing failed, URL bad, network down, or similar', ex);
+            });
+    };
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <h1>CopyFile</h1>
+
+        </header>
+      <main>
+     <button onClick={this.copyFile}>CopyFile</button>
+      </main>
+      <footer>
+          <p>&copy; by Sukhmani </p>
+      </footer>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ week05-ec2-copy-file/server/routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) { 'use strict';
+  res.render('index', { title: 'server' });
+});
+
+router.get('/foo',(request,response) => {
+  const result = {result: 'success'};
+  response.send({ result: 'success' });
+});
+
+module.exports = router;
