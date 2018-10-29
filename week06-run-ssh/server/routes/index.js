8606506038ad75d7280959334479a6397805901c
sukhmani <sukhmani@bellevuecollege.edu>
2018-10-28 23:24:01 -0700

    server-routes-index.js

--- week06-run-ssh/server/routes/index.js
+++ week06-run-ssh/server/routes/index.js
@@ -1,5 +1,46 @@
+const Client = require('ssh2').Client;
 var express = require('express');
 var router = express.Router();
+
+const hostAddress = '54.157.46.121';
+
+let allData = '';
+
+const runCpuInfo = (hostAddress, response) => {
+    var conn = new Client();
+    conn.on('ready', function() {
+        console.log('Client :: ready');
+        conn.exec('~/CpuInfo', function(err, stream) {
+            if (err) throw err;
+            stream
+                .on('close', function(code, signal) {
+                    console.log(
+                        'Stream :: close :: code: ' +
+                            code +
+                            ', signal: ' +
+                            signal
+                    );
+                    conn.end();
+                    response.send({ result: 'success', allData: allData });
+                })
+                .on('data', function(data) {
+                    console.log('STDOUT: ' + data);
+                    allData += data;
+                })
+                .stderr.on('data', function(data) {
+                    console.log('STDERR: ' + data);
+                    allData += data;
+                });
+        });
+    }).connect({
+        host: hostAddress,
+        port: 22,
+        username: 'ubuntu',
+        privateKey: require('fs').readFileSync(
+            process.env.HOME + '/.ssh/new.pem'
+        )
+    });
+};
 
 /* GET home page. */
 router.get('/', function(req, res, next) { 'use strict';
