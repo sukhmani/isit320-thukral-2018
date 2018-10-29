238514c5aca5602a9994040ea62434d004ab4ac9
sukhmani <sukhmani@bellevuecollege.edu>
2018-10-28 23:35:28 -0700

    ssh-runner.js

---
+++ week06-run-ssh/server/routes/ssh-runner.js
@@ -0,0 +1,21 @@
+                    conn.end();
+                    response.send({ result: 'success', allData: allData });
+                })
+                .on('data', function(data) {
+                    console.log('STDOUT: ' + data);
+                    allData += data;
+                })
+                .stderr.on('data', function(data) {
+                    console.log('STDERR: ' + data);
+                    allData += data;
+                });
+        });
+    }).connect({
+        host: hostAddress,
+        port: 22,
+        username: 'ubuntu',
+        privateKey: require('fs').readFileSync(
+            process.env.HOME + '/.ssh/new.pem'
+        )
+    });
+};
