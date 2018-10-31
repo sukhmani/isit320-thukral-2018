fb67ac5408f8d98edfac7f06e0fc14060939d013
sukhmani <sukhmani@bellevuecollege.edu>
2018-10-31 15:07:47 -0700

    prettier

--- week06-system-check/server/routes/script-pusher.js
+++ week06-system-check/server/routes/script-pusher.js
@@ -2,36 +2,37 @@ import { Router } from 'express';
 var router = Router();
 const spawn = require('child_process').spawn;
 
-let allData = "";
+let allData = '';
 
 const copyFile = () => {
+    return new Promise(function(resolve, reject) {
+        console.log('Copy to EC2', process.env.SETUP_LINUXBOX);
 
-    return new Promise(function (resolve, reject) {
+        const pushScript = spawn('scp', [
+            process.env.SETUP_LINUXBOX + '/CpuInfo',
+            'ec2-bc:/home/ubuntu'
+        ]);
 
-        console.log("Copy to EC2", process.env.SETUP_LINUXBOX);
-
-        const pushScript = spawn('scp', [process.env.SETUP_LINUXBOX + '/CpuInfo', 'ec2-bc:/home/ubuntu']);
-
-        pushScript.stdout.on('data', (data) => {
+        pushScript.stdout.on('data', data => {
             console.log(`child stdout:\n${data}`);
             allData += 'PUSH-SCRIPT: ' + data;
             //console.log('PUSH', data);
         });
 
-        pushScript.stderr.on('data', (data) => {
+        pushScript.stderr.on('data', data => {
             console.log(`child stderr:\n${data}`);
             allData += 'PUSH-SCRIPT: ' + data;
             //console.error('PUSH', data);
         });
 
-        pushScript.on('close', (code) => {
+        pushScript.on('close', code => {
             resolve({
                 result: 'success',
                 code: code
             });
         });
 
-        pushScript.on('error', (code) => {
+        pushScript.on('error', code => {
             reject({
                 result: 'error',
                 code: code
@@ -40,8 +41,8 @@ const copyFile = () => {
     });
 };
 
-router.get('/copy-file', (request, response) =>{
-  const result = {result:'success', objName:'script-pusher'};
-  response.send(result);
+router.get('/copy-file', (request, response) => {
+    const result = { result: 'success', objName: 'script-pusher' };
+    response.send(result);
 });
 export default router;
