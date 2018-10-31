a67696ea35e263a4573e5725e7d60a4f98aa94a8
sukhmani <sukhmani@bellevuecollege.edu>
2018-10-30 18:46:52 -0700

    old co

---
+++ week06-run-ssh/client/build/precache-manifest.f5b9b57df5c52f16ec96e28838a8c722.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5e7f921215caf4793d13",
+    "url": "/static/js/main.5e7f9212.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "5e7f921215caf4793d13",
+    "url": "/static/css/main.6a377a7d.chunk.css"
+  },
+  {
+    "revision": "2a90ec4ed0efced813b197d664169b79",
+    "url": "/index.html"
+  }
+];
