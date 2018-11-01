e333a9036d85bbf2ed6d99256e283042e58c77e8
sukhmani <sukhmani@bellevuecollege.edu>
2018-10-31 17:14:13 -0700

    app.js

--- week06-system-check/client/src/App.js
+++ week06-system-check/client/src/App.js
@@ -4,6 +4,42 @@ import './App.css';
 
 class App extends Component {
     render() {
+        const radioWeb = (
+            <div className="container">
+            <form onSubmit={this.handleSubmit}>
+    <fieldset>
+        <div className="elf-form-field">
+
+            <legend>Services</legend>
+            <input
+        type="radio"
+        name="app-choice"
+        data-endpoint="0"
+        value="CpuInfo"
+        id="elf-radio-cpu"
+        onChange={this.handleChange}
+        />
+        <label htmlFor="elf-radio-cpu">CpuInfo</label>
+
+            <input
+        type="radio"
+        name="app-choice"
+        data-endpoint="0"
+        value="VersionCheck"
+        id="elf-radio-version"
+        onChange={this.handleChange}
+        />
+        <label htmlFor="elf-radio-version">Version Info</label>
+
+        </div>
+
+        <div className="form-group">
+            <button type="submit" className="btn btn-primary">Run System Script</button>
+        </div>
+        </fieldset>
+        </form>
+        </div>
+    );
         return (
             <div className="App">
                 <header className="App-header">
