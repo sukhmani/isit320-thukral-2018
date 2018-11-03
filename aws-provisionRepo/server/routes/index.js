3e3fb2a2b43c95d724b16cf544b6e6484cef1ad4
sukhmani <sukhmani@bellevuecollege.edu>
2018-11-03 15:44:55 -0700

    server routes index.js  objects return response response.send({result: 'success'});

--- aws-provisionRepo/server/routes/index.js
+++ aws-provisionRepo/server/routes/index.js
@@ -11,39 +11,39 @@ router.get('/', function(req, res, next) { 'use strict';
 router.get('/awsEducate', function(request, response) {
   const awsInstanceParams = getAwsInstanceParams.awsEducate();
   createInstance(awsInstanceParams);
-    //response.send(message);
+    response.send({result: 'success'});
 });
 
 /* Set up a route called foo. */
 router.get('/createWithAwsStandardAccount', function(request, response) {
     const awsInstanceParams = getAwsInstanceParams.createWithAwsStandardAccount();
     createInstance(awsInstanceParams);
-    //response.send(message);
+    response.send({result: 'success'});
 });
 module.exports = router;
 /* Set up a route called foo. */
 router.get('/associateElasticIp', function(request, response) {
     const awsInstanceParams = getAwsInstanceParams.associateElasticIp();
     createInstance(awsInstanceParams);
-    //response.send(message);
+    response.send({result: 'success'});
 });
 module.exports = router;
 /* Set up a route called foo. */
 router.get('/copyGetStarted', function(request, response) {
     const awsInstanceParams = getAwsInstanceParams.copyGetStarted();
     createInstance(awsInstanceParams);
-    //response.send(message);
+    response.send({result: 'success'});
 });
 /* Set up a route called foo. */
 router.get('/runGetStarted', function(request, response) {
     const awsInstanceParams = getAwsInstanceParams.runGetStarted();
     createInstance(awsInstanceParams);
-    //response.send(message);
+    response.send({result: 'success'});
 });
 /* Set up a route called foo. */
 router.get('/removeKnownHost', function(request, response) {
     const awsInstanceParams = getAwsInstanceParams.removeKnownHost();
     createInstance(awsInstanceParams);
-    //response.send(message);
+    response.send({result: 'success'});
 });
 module.exports = router;
