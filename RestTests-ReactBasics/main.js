c4bef44c604c8d34da361ed910852ff04813dcf9
sukhmani <sukhmani@bellevuecollege.edu>
2018-11-04 01:19:05 -0700

    cp -r Week02-ReactBasics/ RestTests-ReactBasics

---
+++ RestTests-ReactBasics/main.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ReactSimple, {foo} from './react-simple';
+
+ReactDOM.render(
+    <ReactSimple/>,
+    document.getElementById("root")
+);
+
