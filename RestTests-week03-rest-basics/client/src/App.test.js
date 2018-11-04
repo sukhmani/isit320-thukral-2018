f7167cf3e39683c99b997f95fd426e358d83ca61
sukhmani <sukhmani@bellevuecollege.edu>
2018-11-04 01:08:59 -0800

    app test - imprt react,reactapp,dom

--- RestTests-week03-rest-basics/client/src/App.test.js
+++ RestTests-week03-rest-basics/client/src/App.test.js
@@ -1,3 +1,8 @@
+import {shallow} from 'enzyme';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
