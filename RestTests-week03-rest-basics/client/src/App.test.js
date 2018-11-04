d86115c54af33ea82732f7055f41fc3b2fc99ace
sukhmani <sukhmani@bellevuecollege.edu>
2018-11-04 01:23:05 -0800

    app test.js 2 render wiyhout crashing

--- RestTests-week03-rest-basics/client/src/App.test.js
+++ RestTests-week03-rest-basics/client/src/App.test.js
@@ -1,3 +1,4 @@
+import elfDebugEnzyme from './ElfDebugEnzyme';
 import {shallow} from 'enzyme';
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
@@ -11,4 +12,20 @@ it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+describe('rest basic tests', function() {    
+
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<App/>, div);
+    });
+
+    it('renders initial value of paragraph with state.nine', () => {
+        const wrapper = shallow(<App/>);
+        const unknown = <p className="App-intro">file: unknown</p>;
+        elfDebugEnzyme.getLast(wrapper, 'p', true);
+        expect(wrapper.contains(unknown)).toEqual(true);
+    });
+
 });
