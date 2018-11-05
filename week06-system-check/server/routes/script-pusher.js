5821be1464b989cf296b941e7034385db6ecb7c4
sukhmani <sukhmani@bellevuecollege.edu>
2018-11-04 19:19:41 -0800

    scriptpusher

--- week06-system-check/server/routes/script-pusher.js
+++ week06-system-check/server/routes/script-pusher.js
@@ -6,6 +6,44 @@ var router = express.Router();
   res.render('index', { title: 'server' });
 });*/
 
+const spawn = require('child_process').spawn;
 
+let allData = "";
+
+const copyFile = () => {
+
+    return new Promise(function (resolve, reject) {
+
+        console.log("Copy to EC2", process.env.SETUP_LINUXBOX);
+
+        const pushScript = spawn('scp', [process.env.SETUP_LINUXBOX + '/CpuInfo', 'ec2-bc:/home/ubuntu']);
+
+        pushScript.stdout.on('data', (data) => {
+            console.log(`child stdout:\n${data}`);
+            allData += 'PUSH-SCRIPT: ' + data;
+            //console.log('PUSH', data);
+        });
+
+        pushScript.stderr.on('data', (data) => {
+            console.log(`child stderr:\n${data}`);
+            allData += 'PUSH-SCRIPT: ' + data;
+            //console.error('PUSH', data);
+        });
+
+        pushScript.on('close', (code) => {
+            resolve({
+                result: 'success',
+                code: code
+            });
+        });
+
+        pushScript.on('error', (code) => {
+            reject({
+                result: 'error',
+                code: code
+            });
+        });
+    });
+};
 
 module.exports = router;
