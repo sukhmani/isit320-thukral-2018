1f4b2d58e76b26c97756d0d124030b21c4f24658
sukhmani <sukhmani@bellevuecollege.edu>
2018-11-05 16:06:05 -0800

    --verbose test -- package.json

--- week06-system-check/client/src/App.test.js
+++ week06-system-check/client/src/App.test.js
@@ -1,9 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {configure, shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import elfDebugEnzyme from './ElfDebugEnzyme';
 
-it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
-    ReactDOM.unmountComponentAtNode(div);
+configure({ adapter: new Adapter() });
+
+describe('rest basic tests', function() {
+
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<App/>, div);
+    });
+    it('renders initial value of paragraph with state.nine', () => {
+        const wrapper = shallow(<App/>);
+        console.log('Tester',wrapper.find('h1')debug());
+    });
+    it('renders initial value of paragraph with state.nine', () => {
+        const wrapper = shallow(<App/>);
+        const unknown = <p className="App-intro">file: unknown</p>;
+        elfDebugEnzyme.getLast(wrapper, 'p', true);
+        expect(wrapper.contains(unknown)).toEqual(true);
+    });
+
 });
+
+
+
