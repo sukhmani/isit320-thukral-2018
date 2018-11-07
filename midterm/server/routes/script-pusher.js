bcbc284110c5cb9281b8ced70ef6f3030265d693
sukhmani <sukhmani@bellevuecollege.edu>
2018-11-06 22:54:24 -0800

    script-pusher.js

---
+++ midterm/server/routes/script-pusher.js
@@ -0,0 +1,97 @@
+var express = require('express');
+
+var router = express.Router();
+
+
+
+/* GET home page. */
+
+/*router.get('/', function(req, res, next) { 'use strict';
+
+  res.render('index', { title: 'server' });
+
+});*/
+
+
+
+const spawn = require('child_process').spawn;
+
+
+
+let allData = "";
+
+
+
+const copyFile = () => {
+
+
+
+    return new Promise(function (resolve, reject) {
+
+
+
+        console.log("Copy to EC2", process.env.SETUP_LINUXBOX);
+
+
+
+        const pushScript = spawn('scp', [process.env.SETUP_LINUXBOX + '/CpuInfo', 'ec2-bc:/home/ubuntu']);
+
+
+
+        pushScript.stdout.on('data', (data) => {
+
+            console.log(`child stdout:\n${data}`);
+
+            allData += 'PUSH-SCRIPT: ' + data;
+
+            //console.log('PUSH', data);
+
+        });
+
+
+
+        pushScript.stderr.on('data', (data) => {
+
+            console.log(`child stderr:\n${data}`);
+
+            allData += 'PUSH-SCRIPT: ' + data;
+
+            //console.error('PUSH', data);
+
+        });
+
+
+
+        pushScript.on('close', (code) => {
+
+            resolve({
+
+                result: 'success',
+
+                code: code
+
+            });
+
+        });
+
+
+
+        pushScript.on('error', (code) => {
+
+            reject({
+
+                result: 'error',
+
+                code: code
+
+            });
+
+        });
+
+    });
+
+};
+
+
+
+module.exports = router;
