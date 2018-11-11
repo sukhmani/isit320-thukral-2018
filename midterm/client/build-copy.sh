fbae144ad3441ea84276ad0b8fbd2b566adde176
sukhmani <sukhmani@bellevuecollege.edu>
2018-11-10 19:46:07 -0800

    build-cpy

--- midterm/client/build-copy.sh
+++ midterm/client/build-copy.sh
@@ -1,3 +1,22 @@
+
 #! /usr/bin/env bash
-SERVER_DIR=../server/public/
+
+SERVER_DIR='../server/public/'
+echo ${SERVER_DIR}/precache-manifest*.js
+
 #that points at the public directory in the server folder
+#! /usr/bin/env bash
+
+
+echo $SERVER_DIR
+function copyNew() {
+
+npm run build
+cp -r build/* $(SERVER_DIR)}
+
+npm run buid
+function()
+
+rm $(SERVER_DIR)/precache-manifest*.js
+
+rm -r $(SERVER_DIR)/static
