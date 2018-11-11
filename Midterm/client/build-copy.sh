f34db1f43c3b9e9e264aff55240f1b9ae38fbd5c
sukhmani <sukhmani@bellevuecollege.edu>
2018-11-11 15:47:16 -0800

    build-copy redone

--- Midterm/client/build-copy.sh
+++ Midterm/client/build-copy.sh
@@ -1,5 +1,28 @@
 
 #! /usr/bin/env bash
+RED='\033[0;31m'
+LIGHT_RED='\033[1;31m'
+LIGHT_GREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[1;36m'
+NC='\033[0m' # No Color
+
+while true; do
+    message "Menu"    
+    echo -e "$LIGHT_GREEN  a) Delete Old Files and Run Build"
+    echo -e "$LIGHT_GREEN  b) Only Build"
+    echo -e "$LIGHT_GREEN  c) Only Delete"
+    echo -e "$LIGHT_RED  x) Exit (You should source .bashrc when done)"
+    echo -e "\n$NC"
+    read -p "Please make a selection: " eotuyx
+    case $eotuyx in
+        [Aa]* ) runAll false; continue;;
+        [Bb]* ) copyNew; continue;;
+        [Cc]* ) deleteOld; continue;;
+        [XxQq]* ) break;;
+        * )  -e "\n$NC" + "Please answer with a, b, c or x (or q).";;
+    esac
+done
 
 SERVER_DIR='../server/public/'
 echo ${SERVER_DIR}/precache-manifest*.js
