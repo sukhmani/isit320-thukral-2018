0cc87cf6bbe7624db8d77277951e5ea2b5d65a82
sukhmani <sukhmani@bellevuecollege.edu>
2018-11-11 19:17:54 -0800

    routes index updated with script pusher and ssh-runner

--- Midterm/server/app.js
+++ Midterm/server/app.js
@@ -6,6 +6,10 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var ssh-runner  = require('./routes/ssh-runner');
+var script-pusher = require('./routes/script-pusher');
+
+
 
 var app = express();
 
@@ -21,6 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/script-pusher', script-pusher);
+app.use('/ssh-runner', ssh-runner);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- Midterm/server/routes/index.js
+++ Midterm/server/routes/index.js
@@ -1,10 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+router.use(express.static('public'));
 /* GET home page. */
-router.get('/', function(req, res) {
-    'use strict';
-    res.render('index', { title: 'server' });
-});
 
-module.exports = router;
+//const  routes = require ('./routes');
+
+module.exports = () => {
+    router.get('/', function(req, res) {
+        'use strict';
+        res.render('index', { title: 'server' });
+    });
+};
+
+app.listen(PORT, () =>{
+    console.log('${PORT}');
+});
