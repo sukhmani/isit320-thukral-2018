11114eca1e8929d427b92b85e454ce3dc7316d7f
sukhmani <sukhmani@bellevuecollege.edu>
2018-11-12 22:45:52 -0800

    errorswith build

--- Midterm/client/build-copy.sh
+++ Midterm/client/build-copy.sh
@@ -1,5 +1,5 @@
-
 #! /usr/bin/env bash
+
 RED='\033[0;31m'
 LIGHT_RED='\033[1;31m'
 LIGHT_GREEN='\033[1;32m'
@@ -7,8 +7,51 @@ YELLOW='\033[1;33m'
 BLUE='\033[1;36m'
 NC='\033[0m' # No Color
 
+
+
+${SERVER_DIR} '../server/public/'
+echo ${SERVER_DIR}/precache-manifest*.js
+#that points at the public directory in the server folder
+
+echo ${SERVER_DIR}
+function copyNew() {
+    npm run build
+    cp -r build/* ${SERVER_DIR}/.
+}
+
+
+
+function deleteOld() {
+#asset-manifest.json
+#favicon.ico
+#index.html
+#manifest.json
+#precache-manifest.2efd1de520c30948b299e17d59c75fef.js
+#service-worker.js
+#static
+    rm -v ${SERVER_DIR}/precache-manifest*.js
+ rm -v -r ${SERVER_DIR}'/static'
+rm -v ${SERVER_DIR}/asset-manifest.json
+rm -v ${SERVER_DIR}/favicon.ico
+rm -v ${SERVER_DIR}/index.html
+rm -v ${SERVER_DIR}/manifest.json
+rm -v ${SERVER_DIR}/precache-manifest.2efd1de520c30948b299e17d59c75fef.js
+rm -v ${SERVER_DIR}/service-worker.js
+
+}
+
+function runAll() {
+     copyNew
+     deleteOld
+}
+
+#copyNew
+#deleteOld
+#runAll
+
+
 while true; do
-    message "Menu"    
+    message "Menu"
     echo -e "$LIGHT_GREEN  a) Delete Old Files and Run Build"
     echo -e "$LIGHT_GREEN  b) Only Build"
     echo -e "$LIGHT_GREEN  c) Only Delete"
@@ -23,36 +66,3 @@ while true; do
         * )  -e "\n$NC" + "Please answer with a, b, c or x (or q).";;
     esac
 done
-
-SERVER_DIR='../server/public/'
-echo ${SERVER_DIR}/precache-manifest*.js
-#that points at the public directory in the server folder
-
-echo ${SERVER_DIR}
-function copyNew() {
-    npm run build
-    cp -r build/* ${SERVER_DIR}/.
-}
-
-function deleteOld() {
-#asset-manifest.json
-#favicon.ico
-#index.html
-#manifest.json
-#precache-manifest.2efd1de520c30948b299e17d59c75fef.js
-#service-worker.js
-#static
-    rm -v ${SERVER_DIR}/precache-manifest*.js
-    rm -v -r ${SERVER_DIR}'/static'
-rm -v ${SERVER_DIR}/asset-manifest.json
-rm -v ${SERVER_DIR}/favicon.ico
-rm -v ${SERVER_DIR}/index.html
-rm -v ${SERVER_DIR}/manifest.json
-rm -v ${SERVER_DIR}/precache-manifest.2efd1de520c30948b299e17d59c75fef.js
-rm -v ${SERVER_DIR}/service-worker.js
-}
-
-function runAll() {
-    function: copyNew
-    function: deleteOld
-}
