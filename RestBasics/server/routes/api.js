bb502af05b8e0e4b5a7744bf487ea6e5df88c006
sukhmani <sukhmani@bellevuecollege.edu>
2018-11-25 16:00:48 -0800

    =

--- RestBasics/server/routes/api.js
+++ RestBasics/server/routes/api.js
@@ -1,10 +1,13 @@
+//import {Router} from "express";
+
 var express = require('express');
 var router = express.Router();
 
-/* Set up a route called foo. */
-router.get('/foo', function(request, response) {
+
+/* Set up a route called api. */
+router.get('/api', function(request, response) {
     var message = { 'result': 'success', 'status': 'bar', 'file': 'api.js' };
-    console.log('Foo called:\n' + JSON.stringify(message, null, 4));
+    console.log('api called:\n' + JSON.stringify(message, null, 4));
     response.send(message);
 });
 
