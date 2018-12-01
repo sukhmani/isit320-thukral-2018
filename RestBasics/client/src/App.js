a5e9e5bebd2266b045a05e45cf3e41f7c502fe3f
sukhmani <sukhmani@bellevuecollege.edu>
2018-11-30 18:49:59 -0800

    updated

--- RestBasics/client/src/App.js
+++ RestBasics/client/src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
     constructor() {
         super();
         this.state = {
-            file: 'File name will be placed here.',
+            file: '',
             status: 'waiting for server'
         };
     }
@@ -21,19 +21,19 @@ class App extends Component {
                 return response.json();
             })
             .then(function(json) {
-                console.log('parsed json', json);
-                that.setState(foo => (json));
+                console.log('data', json);
+                that.setState(api => (json));
             })
             .catch(function(ex) {
                 console.log('parsing failed, URL bad, network down, or similar', ex);
             });
     };
-
+ data=[{"result":"success","foo":"bar","file":"api.js", "status":"done!"}];
     render() {
         return (
             <div className="App">
                 <p className="App-intro">
-                    state: {this.state.status} file: {this.state.file}
+                    state: {this.state.status} file: {this.state.file} result: {this.state.result} foo: {this.state.foo}
                 </p>
                 <button onClick={this.queryServer}>Bar</button>
             </div>
