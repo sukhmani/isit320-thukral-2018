877b85aa6b20607001b2e6a6bb720a1ffd7b27c2
sukhmani <sukhmani@bellevuecollege.edu>
2018-12-02 21:49:56 -0800

    aws

---
+++ week06AWSSSH/client/build/precache-manifest.8a8d138f30c8910ceed3e125a64adfb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e2d710351a5cbcdffd15",
+    "url": "/static/js/main.e2d71035.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "e2d710351a5cbcdffd15",
+    "url": "/static/css/main.ee02b5a2.chunk.css"
+  },
+  {
+    "revision": "b5136eb4615e4f3a4a7e11d36014eb0d",
+    "url": "/index.html"
+  }
+];
