74ef8241773928cf67be1da0f5f43460c1b765e5
sukhmani <sukhmani@bellevuecollege.edu>
2018-12-06 17:00:59 -0800

    -

--- aws-provisionRepo/client/src/ElfHeader.js
+++ aws-provisionRepo/client/src/ElfHeader.js
@@ -39,20 +39,7 @@ class ElfHeader extends Component{
 
 
 export default ElfHeader;
-=======
-class ElfHeader extends Component{
-    render() {
-        return (
-            <div className="App">
-                <header>
-                    <h1> Aws Provision</h1>
-                </header>
-            </div>
-        );
 
-    }
-}
 
-    export default ElfHeader;
 
->>>>>>> 7209da5927cc71906a8e27fb765a7bbe89acd0b0
+
