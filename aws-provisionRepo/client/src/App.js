e4b2f383d51c3b008a3d117b638af5ff36ff94f9
sukhmani <sukhmani@bellevuecollege.edu>
2018-12-06 23:45:53 -0800

    -

--- aws-provisionRepo/client/src/App.js
+++ aws-provisionRepo/client/src/App.js
@@ -8,179 +8,78 @@ import ElfHeader from './ElfHeader';
 
 class App extends Component {
     runScript = (path, script) => {
-
         const that = this;
-
         if (!script) {
-
             return;
-
         }
-
         fetch(path + script)
-
             .then(function(response) {
-
                 return response.json();
-
             })
-
             .then(function(json) {
-
                 console.log('allData', json.allData);
-
                 console.log('result', json.result);
-
                 console.log('code', json.code);
-
                 console.log('error', json.error);
-
                 let info = '';
-
                 if (json.result === 'error') {
-
                     info = json.error;
-
                 } else if (script === 'CpuInfo') {
-
                     var regex1 = RegExp('model name.*', 'g');
-
                     let array1 = regex1.exec(json.allData);
-
                     while (array1 !== null) {
-
                         info += array1[0] + '\n';
-
                         console.log(`Found ${array1[0]}.`);
-
                         array1 = regex1.exec(json.allData);
-
                     }
-
                 } else {
-
                     info = json.allData;
-
                 }
-
                 that.setState({ allData: info });
-
             })
-
             .catch(function(ex) {
-
                 console.log(
-
                     'parsing failed, URL bad, network down, or similar',
-
                     ex
-
                 );
-
             });
-
     };
 
     handleChange = event => {
-
         const selectedValue = event.target.value;
-
         const endPointIndex = event.target.getAttribute('data-endpoint');
-
         console.log('HANDLE CHANGE', selectedValue);
-
         this.setState({
-
             ...this.state,
-
             selectedValue: selectedValue,
-
             endPointIndex: endPointIndex
-
         });
-
     };
 
     handleSubmit = event => {
-
         this.setState({ allData: '' });
-
         console.log('A name was submitted: ', this.state);
-
         this.runScript(
-
             this.dataEndPoints[this.state.endPointIndex],
-
             this.state.selectedValue
-
         );
-
         event.preventDefault();
-
     };
     constructor(props) {
-
         super(props);
-
         this.dataEndPoints = [
 
             '/script-pusher/run-script?script=',
-
             '/ssh-runner/runCpuInfo?script=',
-
-            //'/script-pusher/run-system-tool?script=',
-
             '/ssh-runner/runCpuUptime?script=',
-
             '/ssh-runner/runCpuInfo?script='
-
         ];
-
         this.state = {
-
             allData: '',
-
             selectedValue: '',
-
             endPointIndex: 0
-
         };
-
     }
-
-   /* constructor(props) {
-
-        super(props);*/
-
-        /*   this.dataEndPoints = [
-
-            '/script-pusher/run-script?script=',
-
-            '/ssh-runner/runCpuInfo?script=',
-
-            //'/script-pusher/run-system-tool?script=',
-
-            '/ssh-runner/runCpuUptime?script=',
-
-            '/ssh-runner/runCpuInfo?script='
-
-        ];*/
-
-        /*this.state = {
-
-            value: null,
-
-            users:[] ,
-
-            endPointIndex: 0
-
-
-
-        };*/
-
-    //}
-
-
     state = {users: [],
         value: [],
         created: [],
@@ -188,143 +87,65 @@ class App extends Component {
         Copied:[],
         Started:[],
         Reboot:[]
-
     }
-
     a = ()=> {
         fetch('./users')
-
             .then(res => res.json())
             .then(users => this.setState({ users }));
     }
-
     createEducate = () => {
-
         const that = this;
-
         fetch('aws/createEducate')
-
             .then(function(response) {
-
                 return response.json();
-
             })
-
             .then(function(json) {
-
                 console.log('parsed json', json);
-
                 const q = Object.keys(json.q).map(function(key) {
-
                     return [key, ' ', json.q[key], ' '];
-
                 });
-
                 that.setState({
-
-                    /*allData: json.title,
-
-                    selectedValue: query,
-
-                    endPointIndex: json.query.count,*/
-
                     user: json.user,
-
                     value: q,
                     params: JSON.stringify(json.params),
-
-
                 });
-
             })
-
             .catch(function(ex) {
-
                 console.log(
-
                     'parsing failed, URL bad, network down, or similar',
-
                     ex
-
                 );
-
             });
-
     };
 
     createWithAwsStandardAccount = () => {
-
         const that = this;
-
         fetch('/createWithAwsStandardAccount?bar=qux&count=5')
-
             .then(function(response) {
-
                 return response.json();
-
             })
-
             .then(function(json) {
-
                 console.log('parsed json', json);
-
                 const q = Object.keys(json.q).map(function(key) {
-
                     return [key, ' ', json.q[key], ' '];
-
                 });
-
                 that.setState({
                     allData: json.allData,
-
                     selectedValue: q,
-
                     endPointIndex: json.q.endPointIndex,
-
-                    /*title: json.title,
-
-                    query: query,
-
-                    queryBar: json.query.bar,
-
-                    queryCount: json.query.count,
-
-                    params: JSON.stringify(json.params),*/
-
                 });
 
             })
-
             .catch(function(ex) {
-
                 console.log(
-
                     'parsing failed, URL bad, network down, or similar',
-
                     ex
-
                 );
-
             });
-
     };
-
-
     render() {
-
         return (
-
-
-
-
             <div className="App">
-
-
-
-
-
-
-
                 <ElfHeader />
                 <main>
                     <button
@@ -384,35 +205,14 @@ class App extends Component {
                     <button onClick={this.removeKnownHost}>get instance status</button>
                     <button onClick={() => this.setState({ Reboot: '    :Done '})}>reboot instance</button>
 
-{/*<button onClick={
-    this.state.users.map(users =>
-     key={users.id}>{users.username}
-)
-}>users</button>*/}
-
                 </main>
-
-
-
                 <footer>
-
-
-
-
-
                     <p>&copy; by Sukhmani t</p>
 
                 </footer>
-
             </div>
-
         );
-
     }
-
 }
-
-
-
 export default App;
 
